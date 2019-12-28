3f1b67876ed9ae0b7b219fbbaf48f5bd2dde6228
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-28 16:28:29 +0530

    Boxing and Unboxing

--- src/com/navishkakularathna/Main.java
+++ src/com/navishkakularathna/Main.java
@@ -33,5 +33,19 @@ public class Main {
         for(int i=0; i<=10; i++){
             System.out.println(i + " --> " + intArrayList.get(i).intValue());
         }
+
+        Integer myIntValue = 56;
+        int myInt = myIntValue.intValue();
+
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for(double dbl=0.0; dbl<=10.0; dbl += 0.5){
+            myDoubleValues.add(dbl);
+        }
+
+        for (int i=0; i<myDoubleValues.size(); i++){
+            double value = myDoubleValues.get(i);
+            System.out.println(i + " --> " + value);
+        }
+
     }
 }
