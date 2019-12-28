5dfff2dd383804597430e7416ea484d35547c616
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-28 14:37:01 +0530

    IntClass class created

---
+++ src/com/navishkakularathna/Main.java
@@ -0,0 +1,25 @@
+package com.navishkakularathna;
+
+import java.util.ArrayList;
+
+class IntClass{
+    private int myValue;
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+    public int getMyValue() {
+        return myValue;
+    }
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //ArrayList<IntClass>
+
+    }
+}
